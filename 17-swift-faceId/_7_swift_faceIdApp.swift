5731da94f33fab493f9b8a5591a25ab2035ffc98
Liu Ziyi <ziyi.liu02@gmail.com>
2023-09-06 19:20:19 +0800

    Initial Commit

---
+++ 17-swift-faceId/_7_swift_faceIdApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7_swift_faceIdApp.swift
+//  17-swift-faceId
+//
+//  Created by Liu Ziyi on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct _7_swift_faceIdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
