d09ff564d2b96effaa7508e09c18333344e973cc
Liu Ziyi <ziyi.liu02@gmail.com>
2023-09-07 14:44:59 +0800

    - Create AuthenticationManager

--- 17-swift-faceId/Views/LoginView.swift
+++ 17-swift-faceId/Views/LoginView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @EnvironmentObject var authenticationManager: AuthenticationManager
+    
     var body: some View {
         VStack(spacing: 40) {
             Title()
             
-            PrimaryButton(image: "faceid",text: "Login with FaceID")
+            switch authenticationManager.biometryType {
+            case .faceID:
+                PrimaryButton(image: "faceid", text: "Login with FaceID")
+            case .touchID:
+                PrimaryButton(image: "touchid", text: "Login with TouchID")
+            default:
+                PrimaryButton(image: "person.fill", text: "Login with your credentials")
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(LinearGradient(colors: [.blue, .purple], startPoint: .topLeading, endPoint: .bottomTrailing))
@@ -22,5 +32,6 @@ struct LoginView: View {
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
+            .environmentObject(AuthenticationManager())
     }
 }
